a3c5d100b2939bc75a4aac48c4f0fa379e3ef7f1
simeunseo <rkdcjfehowl@naver.com>
2022-07-07 00:54:13 +0900

    기록 기능 완료

--- Javascript 스톱워치 과제/stopwatch.js
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -8,11 +8,12 @@ const lapseSecond = document.querySelector(".time .second");
 const record = document.querySelector(".record ul");
 let intervalID;
 let newRecord;
-const selectAllBtn = document.querySelector(".select-all");
-let selectOneBtn = document.querySelectorAll(".select");
-let recordAll;
-let newSelect;
+const selectAllBtn = document.querySelector(".check-all"); //전체 선택 버튼
+let btnList = document.querySelectorAll(".check"); //모든 기록들의 체크 버튼 리스트
+const trashBtn = document.querySelector(".trash");
+let allCheckedFlag = 0;
 
+//타이머 start버튼 기능
 startBtn.addEventListener("click", function () {
   clearInterval(intervalID);
   intervalID = setInterval(function () {
@@ -26,6 +27,7 @@ startBtn.addEventListener("click", function () {
   }, 10);
 });
 
+//타이머 stop버튼 기능
 stopBtn.addEventListener("click", function () {
   clearInterval(intervalID);
   newRecord = document.createElement("li");
@@ -39,28 +41,16 @@ stopBtn.addEventListener("click", function () {
         <div class="millisecond">00</div>
     </div>
   `;
+  //현재 기록 가져와서 기록하기
   record.appendChild(newRecord);
   newRecord.lastElementChild.lastElementChild.innerText =
     lapseMillisecond.innerText;
   newRecord.lastElementChild.firstElementChild.innerText =
     lapseSecond.innerText;
-
-  /*
-  selectOneBtn = document.querySelectorAll(".select");
-  selectOneBtn.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      if (btn.firstElementChild.style.display == "none") {
-        btn.firstElementChild.style.display = "unset";
-        btn.lastElementChild.style.display = "none";
-      } else {
-        btn.firstElementChild.style.display = "none";
-        btn.lastElementChild.style.display = "unset";
-      }
-    });
-  });
-  */
+  btnList = document.querySelectorAll(".check");
 });
 
+//타이머 reset버튼 기능
 resetBtn.addEventListener("click", function () {
   millisecond = 0;
   second = 0;
@@ -69,24 +59,37 @@ resetBtn.addEventListener("click", function () {
   clearInterval(intervalID);
 });
 
-/*
+//기록 전체 선택 기능
 selectAllBtn.addEventListener("click", function () {
-  if (selectAllBtn.firstElementChild.style.display == "none") {
-    //이미 선택된 상태라면 선택해제한다
-    selectAllBtn.firstElementChild.style.display = "unset";
-    selectAllBtn.lastElementChild.style.display = "none";
-    for (let i = 0; i < recordAll.length; i++) {
-      recordAll.item(i).firstElementChild.style.display = "unset";
-      recordAll.item(i).lastElementChild.style.display = "none";
+  if (selectAllBtn.checked == true) {
+    //체크가 안되어있다면 체크한다
+    selectAllBtn.checked = true;
+    for (let j = 0; j < btnList.length; j++) {
+      btnList[j].checked = true;
     }
+    allCheckedFlag = 1;
   } else {
-    selectAllBtn.firstElementChild.style.display = "none";
-    selectAllBtn.lastElementChild.style.display = "unset";
-    recordAll = document.querySelectorAll(".record .select");
-    for (let i = 0; i < recordAll.length; i++) {
-      recordAll.item(i).firstElementChild.style.display = "none";
-      recordAll.item(i).lastElementChild.style.display = "unset";
+    //이미 체크되어있다면 모두 해제한다
+    selectAllBtn.checked = false;
+    for (let j = 0; j < btnList.length; j++) {
+      btnList[j].checked = false;
+    }
+    allCheckedFlag = 0;
+  }
+});
+
+for (let i = 0; i < btnList.length; i++) {
+  if (btnList[i].checked == false) {
+    allCheckedFlag = 0;
+    selectAllBtn.checked = false;
+  }
+}
+
+trashBtn.addEventListener("click", function () {
+  for (let k = 0; k < btnList.length; k++) {
+    //체크된 기록이라면
+    if (btnList[k].checked == true) {
+      btnList[k].parentElement.parentElement.remove();
     }
   }
 });
-*/
