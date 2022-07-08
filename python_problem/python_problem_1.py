15601afebfd298792f6dd042bb2f47861b5aff58
simeunseo <rkdcjfehowl@naver.com>
2022-07-08 17:48:07 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,23 @@
+def checkInput(num):
+    if num.isdigit(): #입력값이 숫자라면
+        #정수인지 확인한다
+        if float(num) - int(num) != 0: #정수가 아니라면
+            print('정수를 입력하세요')
+            return 1
+        else : #정수라면
+            if int(num)<1 or int(num)>3 : #정수이지만 범위에 맞지 않는다면
+                print('1,2,3 중 하나를 입력하세요')
+                return 2
+            else : #정수이면서 범위에 맞는다면
+                return 0
+    else : #입력값이 숫자가 아니라면
+        print('정수를 입력하세요')
+        return 1
+    
+
 num=0
-num=int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
+while True :
+    num=input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
+    if checkInput(num)==0:
+        break
+            
