63ddf01cfa646dc386cae4d2ca92ae165d4a1d90
simeunseo <rkdcjfehowl@naver.com>
2022-07-14 22:54:26 +0900

    review_list.html 적용

--- myMovieReviews/reviews/urls.py
+++ myMovieReviews/reviews/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 app_name = "reviews"
 urlpatterns = [
-    
+    path('review', views.review_list, name='review_list'),
 ]
--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Review
 
-# Create your views here.
+# reviews = Review.objects.all()
+# print(reviews)
+
+def review_list(request):
+    reviews = Review.objects.all()
+    context = {"reviews": reviews}
+    return render(request, template_name='reviews/review_list.html', context=context) 
